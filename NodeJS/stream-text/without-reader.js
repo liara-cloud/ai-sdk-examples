81e792795850464a9dfaaa85f8afc5e59d61851d
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-07-27 16:41:45 +0330

    add NodeJS/stream-text

---
+++ NodeJS/stream-text/without-reader.js
@@ -0,0 +1,23 @@
+// npm i ai @ai-sdk/openai dotenv
+
+import { streamText } from 'ai';
+import { createOpenAI } from '@ai-sdk/openai';
+import { config } from 'dotenv';
+
+config();
+const my_model = createOpenAI({
+  baseURL: process.env.BASE_URL,
+  apiKey: process.env.LIARA_API_KEY,
+});
+ 
+const result = streamText({
+  model: my_model('openai/gpt-4o-mini'),
+  maxTokens: 512,
+  temperature: 0.3,
+  maxRetries: 5,
+  prompt: 'Invent a new holiday and describe its traditions.',
+});
+
+for await (const textPart of result.textStream) {
+  console.log(textPart);
+}
