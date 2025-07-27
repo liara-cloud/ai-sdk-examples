9625edef2f0a39a2b61bf2df101bb916261cc30c
Ali Najmabadizadeh <alinajmabadizadeh2002@gmail.com>
2025-07-27 20:15:20 +0330

    add nodejs/stream-object

--- NodeJS/stream-object/main.js
+++ NodeJS/stream-object/main.js
@@ -1,29 +1,33 @@
 // npm i ai @ai-sdk/openai dotenv zod 
 
-import { streamObject } from 'ai';
+import { generateObject } from 'ai';
 import { createOpenAI } from '@ai-sdk/openai';
 import { config } from 'dotenv';
 import { z } from 'zod';
 
 config();
+
 const my_model = createOpenAI({
   baseURL: process.env.BASE_URL,
   apiKey: process.env.LIARA_API_KEY,
 });
 
-const { partialObjectStream } = streamObject({
+
+const result = await generateObject({
   model: my_model('openai/gpt-4o-mini'),
   schema: z.object({
     recipe: z.object({
       name: z.string(),
-      ingredients: z.array(z.string()),
+      ingredients: z.array(
+        z.object({
+          name: z.string(),
+          amount: z.string(),
+        }),
+      ),
       steps: z.array(z.string()),
     }),
   }),
   prompt: 'Generate a lasagna recipe.',
 });
 
-for await (const partialObject of partialObjectStream) {
-  console.clear();
-  console.log(partialObject);
-}
+console.log(JSON.stringify(result.object.recipe, null, 2));
