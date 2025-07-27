4c11c49c3df7342d6f5ca5e01f37128a258cbd6a
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-07-27 16:24:51 +0330

    add NodeJS/generate-text

---
+++ NodeJS/generate-text/main.js
@@ -0,0 +1,18 @@
+// npm i ai @ai-sdk/openai dotenv
+
+import { generateText } from 'ai';
+import { createOpenAI } from '@ai-sdk/openai';
+import { config } from 'dotenv';
+
+config();
+const my_model = createOpenAI({
+  baseURL: process.env.BASE_URL,
+  apiKey: process.env.LIARA_API_KEY,
+});
+
+const result = await generateText({
+  model: my_model('openai/gpt-4o-mini'),
+  prompt: 'Why is the sky blue?',
+});
+
+console.log(result);
