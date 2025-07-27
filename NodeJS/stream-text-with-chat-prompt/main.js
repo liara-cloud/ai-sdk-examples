e52fc8f633f2399b42ddc4fc697ae8642d8be7e3
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-07-27 16:49:23 +0330

    add NodeJS/stream-text-with-chat-prompt

---
+++ NodeJS/stream-text-with-chat-prompt/main.js
@@ -0,0 +1,35 @@
+// npm i ai @ai-sdk/openai dotenv
+
+import { streamText } from 'ai';
+import { createOpenAI } from '@ai-sdk/openai';
+import { config } from 'dotenv';
+
+config();
+const my_model = createOpenAI({
+  baseURL: process.env.BASE_URL,
+  apiKey: process.env.LIARA_API_KEY,
+});
+ 
+const result = streamText({
+  model: my_model('openai/gpt-4o-mini'),
+  maxTokens: 1024,
+  system: 'You are a helpful chatbot.',
+  messages: [
+    {
+      role: 'user',
+      content: 'Hello!',
+    },
+    {
+      role: 'assistant',
+      content: 'Hello! How can I help you today?',
+    },
+    {
+      role: 'user',
+      content: 'I need help with my computer.',
+    },
+  ],
+});
+
+for await (const textPart of result.textStream) {
+  console.log(textPart);
+}
