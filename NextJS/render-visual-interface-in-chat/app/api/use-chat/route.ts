95c5fb27bb3b3dfb88990bd87a96c257afabf2bc
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-07-27 11:44:22 +0330

    add NextJS/render-visual-interface-in-chat

---
+++ NextJS/render-visual-interface-in-chat/app/api/use-chat/route.ts
@@ -0,0 +1,58 @@
+// npm i ai @ai-sdk/openai zod
+
+import { createOpenAI } from '@ai-sdk/openai'
+import { streamText } from 'ai';
+import { z } from 'zod';
+
+const my_model = createOpenAI({
+
+  baseURL: process.env.BASE_URL!,
+  apiKey: process.env.LIARA_API_KEY!,
+});
+
+export async function POST(request: Request) {
+  const { messages } = await request.json();
+
+  const result = streamText({
+    model: my_model('openai/gpt-4o-mini'),
+
+    messages,
+    tools: {
+      // server-side tool with execute function:
+      getWeatherInformation: {
+        description: 'show the weather in a given city to the user',
+        parameters: z.object({ city: z.string() }),
+        execute: async ({}: { city: string }) => {
+          return {
+            value: 24,
+            unit: 'celsius',
+            weeklyForecast: [
+              { day: 'Monday', value: 24 },
+              { day: 'Tuesday', value: 25 },
+              { day: 'Wednesday', value: 26 },
+              { day: 'Thursday', value: 27 },
+              { day: 'Friday', value: 28 },
+              { day: 'Saturday', value: 29 },
+              { day: 'Sunday', value: 30 },
+            ],
+          };
+        },
+      },
+      // client-side tool that starts user interaction:
+      askForConfirmation: {
+        description: 'Ask the user for confirmation.',
+        parameters: z.object({
+          message: z.string().describe('The message to ask for confirmation.'),
+        }),
+      },
+      // client-side tool that is automatically executed on the client:
+      getLocation: {
+        description:
+          'Get the user location. Always ask for confirmation before using this tool.',
+        parameters: z.object({}),
+      },
+    },
+  });
+
+  return result.toDataStreamResponse();
+}
